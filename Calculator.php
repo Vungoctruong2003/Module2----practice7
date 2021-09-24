814d632840bb149833a2006fbae39c3e8d40638f
Ngọc Trưởng <truongvugh2003@gmail.com>
2021-09-24 11:21:39 +0700

    [Thuc hanh] -Refacter doi ten bien va tach hang

---
+++ Calculator.php
@@ -0,0 +1,31 @@
+<?php
+const ADDITION = '+';
+const SUBTRACTION = '-';
+const MULTIPLICATION = '*';
+const DIVISION = '/';
+
+class Calculator
+{
+    public function calculate($firstOperand, $secondOperand, $operator)
+    {
+        switch ($operator) {
+            case ADDITION:
+                return $firstOperand + $secondOperand;
+            case SUBTRACTION:
+                return $firstOperand - $secondOperand;
+            case MULTIPLICATION:
+                return $firstOperand * $secondOperand;
+            case DIVISION:
+                if ($secondOperand != 0) {
+                    return $firstOperand / $secondOperand;
+                } else {
+                    return "Can not divide by 0";
+                }
+            default:
+                return "Unsupported operation";
+        }
+    }
+}
+
+$calcutor = new Calculator();
+ echo $calcutor->calculate(4,6,SUBTRACTION);
